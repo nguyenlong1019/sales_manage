2da605cece07233311430df7e2654bf93fff1561
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2024-10-23 23:56:56 +0700

    Add Staff buy require view base and url

---
+++ core/views/staff/buy_require.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect 
+
+
+def staff_buy_require_view(request):
+    pass 
+
+
+def staff_add_buy_require_view(request):
+    pass 
+
+
+def staff_edit_buy_require_view(request, pk):
+    pass 
+
+
+def staff_del_buy_require_view(request, pk):
+    pass 
