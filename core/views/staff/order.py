b33bd5bfa355f08fa6201ecdea43c521152ffea1
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2024-10-24 00:02:45 +0700

    Add staff order base view and url

---
+++ core/views/staff/order.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect 
+
+
+def staff_order_view(request):
+    pass 
+
+
+def staff_add_order_view(request):
+    pass 
+
+
+def staff_edit_order_view(request, pk):
+    pass 
+
+
+def staff_del_order_view(request, pk):
+    pass 
