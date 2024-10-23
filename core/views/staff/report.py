5f5f26ae5ba22a05df90a91721f63c260ebe000d
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2024-10-24 00:07:13 +0700

    Add staff base view and url

---
+++ core/views/staff/report.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect 
+
+
+def staff_report_view(request):
+    pass 
+
+
+def staff_edit_report_view(request):
+    pass 
---
+++ core/views/staff/shipping.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect 
+
+
+def staff_shipping_view(request):
+    pass 
+
+
+def staff_add_shipping_view(request):
+    pass 
+
+
+def staff_edit_shipping_view(request, pk):
+    pass 
+
+
+def staff_del_shipping_view(request, pk):
+    pass 
