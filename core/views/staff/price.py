d59c01c16bb4c85cfcae427da36c4ed85eff20aa
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2024-10-24 00:00:01 +0700

    Add staff price view base and url

---
+++ core/views/staff/price.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect 
+
+
+def staff_price_view(request):
+    pass 
+
+
+def staff_add_price_view(request):
+    pass 
+
+
+def staff_edit_price_view(request, pk):
+    pass 
+
+
+def staff_del_price_view(request, pk):
+    pass 
